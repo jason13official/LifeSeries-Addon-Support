215c572cbf30d4f9862b8deea34bd60421156a70
Mat0u5 <matous.danko+github@gmail.com>
2025-08-20 01:01:21 +0200

    Methods in injected interfaces now have a default body since otherwise the mod won't compile.
    {skip_ping}

--- src/main/java/net/mat0u5/lifeseries/utils/interfaces/IServerPlayerEntity.java
+++ src/main/java/net/mat0u5/lifeseries/utils/interfaces/IServerPlayerEntity.java
@@ -3,12 +3,14 @@ package net.mat0u5.lifeseries.utils.interfaces;
 import org.jetbrains.annotations.Nullable;
 
 public interface IServerPlayerEntity {
-    @Nullable Integer ls$getLives();
-    boolean ls$hasAssignedLives();
-    boolean ls$isAlive();
-    void ls$addToLives(int amount);
-    void ls$setLives(int lives);
-    boolean ls$isOnLastLife(boolean fallback);
-    boolean ls$isOnSpecificLives(int check, boolean fallback);
-    boolean ls$isOnAtLeastLives(int check, boolean fallback);
+    String error = "This method should be overridden in the mixin";
+    
+    @Nullable default Integer ls$getLives()                                { throw new UnsupportedOperationException(error); }
+    default boolean ls$hasAssignedLives()                                  { throw new UnsupportedOperationException(error); }
+    default boolean ls$isAlive()                                        { throw new UnsupportedOperationException(error); }
+    default void ls$addToLives(int amount)                              { throw new UnsupportedOperationException(error); }
+    default void ls$setLives(int lives)                                 { throw new UnsupportedOperationException(error); }
+    default boolean ls$isOnLastLife(boolean fallback)                   { throw new UnsupportedOperationException(error); }
+    default boolean ls$isOnSpecificLives(int check, boolean fallback)   { throw new UnsupportedOperationException(error); }
+    default boolean ls$isOnAtLeastLives(int check, boolean fallback)    { throw new UnsupportedOperationException(error); }
 }
